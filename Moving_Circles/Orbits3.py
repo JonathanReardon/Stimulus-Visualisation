9b614bfe70c701994ae2d389c22d2ce121c9b7c8
JonathanReardon <jonathanjreardon@gmail.com>
2019-06-21 04:40:32 +0100

    added orbits3.py

---
+++ Moving_Circles/Orbits3.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python3
+
+from psychopy import visual, core, event, sound
+import random
+import math
+from math import sqrt
+
+win = visual.Window([1000,800], color="white", colorSpace='rgb', rgb=None, allowGUI=True, monitor='testMonitor', units='deg', fullscr=False, screen=2)
+
+rect_x, rect_y = [-5,0]
+Rect = visual.Rect(win, width=10, height=14, fillColor="black", pos=(rect_x, rect_y), units="deg")
+
+def pol_to_cart(distance, angle, x_origin=0, y_origin=0):
+    x=distance*math.cos(math.radians(angle))
+    y=distance*math.sin(math.radians(angle))
+    return x +x_origin, y + y_origin
+    
+def euclidean_distance(x1, y1, x2, y2):
+    distance = sqrt( (x1 - x2)**2 + (y1 - y2)**2 )
+    return distance
+    
+circle_colors = ["red", "green", "orange", "blue", "violet"]
+#circle_colors = ["black", "white"]
+
+center_circle = visual.Circle(win, units = 'deg', radius=.7, fillColor="black", interpolate=True, fillColorSpace='rgb')
+
+circle  = visual.Circle(win, units = 'deg', radius=.7, fillColor="blue", interpolate=True, fillColorSpace='rgb')
+circle_outline  = visual.Circle(win, units = 'deg', radius=6, lineColor="white", interpolate=True, fillColorSpace='rgb')
+
+circle1  = visual.Circle(win, units = 'deg', radius=.7, fillColor="blue", interpolate=True, fillColorSpace='rgb')
+circle1_outline  = visual.Circle(win, units = 'deg', radius=4, lineColor="black", interpolate=True, fillColorSpace='rgb')
+
+circle2  = visual.Circle(win, units = 'deg', radius=.7, fillColor="blue", interpolate=True, fillColorSpace='rgb')
+circle2_outline  = visual.Circle(win, units = 'deg', radius=3, lineColor="white", interpolate=True, fillColorSpace='rgb')
+
+circle3  = visual.Circle(win, units = 'deg', radius=.7, fillColor="blue", interpolate=True, fillColorSpace='rgb')
+circle3_outline  = visual.Circle(win, units = 'deg', radius=2, lineColor="black", interpolate=True, fillColorSpace='rgb')
+
+line_color="red"
+line_connector = visual.Line(win, start=(-4,0), end=(4,0), lineColor=line_color, lineColorSpace='rgb')
+line_connector1 = visual.Line(win, start=(-4,0), end=(4,0), lineColor=line_color, lineColorSpace='rgb')
+line_connector2 = visual.Line(win, start=(-4,0), end=(4,0), lineColor=line_color, lineColorSpace='rgb')
+
+''' circle '''
+circle_position = []
+angle=0
+n_dots = 360*2
+    
+for i in range(n_dots):
+        
+    x,y = pol_to_cart(6,angle, 0, 0)
+    circle_position.append([x,y])
+        
+    angle+=4
+
+''' circle1 '''
+circle1_position = []
+angle=0
+n_dots = 360*2
+    
+for i in range(n_dots):
+        
+    x,y = pol_to_cart(4,angle, 0, 0)
+    circle1_position.append([x,y])
+        
+    angle+=4
+    
+''' circle2 '''
+circle2_position = []
+angle=0
+n_dots = 360*2
+    
+for i in range(n_dots):
+        
+    x,y = pol_to_cart(3,angle, 0, 0)
+    circle2_position.append([x,y])
+        
+    angle+=4
+    
+''' circle3 '''
+circle3_position = []
+angle=0
+n_dots = 360*2
+    
+for i in range(n_dots):
+        
+    x,y = pol_to_cart(2,angle, 0, 0)
+    circle3_position.append([x,y])
+        
+    angle+=4
+    
+routine = True
+while routine == True:
+    
+    Rect.pos = [rect_x, rect_y]
+    Rect.draw()
+    
+    circle.pos=circle_position[0]
+    circle1.pos=circle1_position[-1]
+    circle2.pos=circle2_position[0]
+    circle3.pos=circle3_position[-1]
+    
+    circle.fillColor=random.choice(circle_colors)
+    circle1.fillColor=random.choice(circle_colors)
+    circle2.fillColor=random.choice(circle_colors)
+    circle3.fillColor=random.choice(circle_colors)
+    
+    line_connector.start = circle.pos
+    line_connector.end = circle1.pos
+    
+    line_connector1.start = circle1.pos
+    line_connector1.end = circle2.pos
+    
+    line_connector2.start = circle2.pos
+    line_connector2.end = circle3.pos
+    
+    center_circle.draw()
+    
+    circle_outline.draw()
+    circle.draw()
+    
+    circle1_outline.draw()
+    circle1.draw()
+    
+    circle2_outline.draw()
+    circle2.draw()
+    
+    circle3_outline.draw()
+    circle3.draw()
+    
+    line_connector.draw()
+    line_connector1.draw()
+    line_connector2.draw()
+   
+    win.flip()
+    
+    circle_position.pop(0)
+    circle1_position.pop(-1)
+    circle2_position.pop(0)
+    circle3_position.pop(-1)
+    
+    rect_x = rect_x+.015
+
+win.close()
+core.quit()
+    
